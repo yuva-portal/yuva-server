a314b42ad18f664b060e408c84a15661428213ab
Anurag <anuragtiw112@gmail.com>
2023-07-22 15:58:22 +0530

    Updated port

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use("/api/admin/auth", require("./api/routes/admin.js"));
 
 app.use("/api/public", require("./api/routes/public.js"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
 
