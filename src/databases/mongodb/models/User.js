fff9f892bd99ffe24fbb1ac51bf0dee5df5cdc06
Pulkit Gupta <pulkitgupta95700@gmail.com>
2024-02-06 12:16:41 +0530

    vert data updated

--- src/databases/mongodb/models/User.js
+++ src/databases/mongodb/models/User.js
@@ -33,9 +33,9 @@ const UserSchema = mongoose.Schema(
   {
     fName: {
       type: String,
-    //   required: [true, "First name is required"],
-    //   minLength: [1, "First name is too short"],
-    //   maxLength: [60, "First name is too long"],
+      //   required: [true, "First name is required"],
+      //   minLength: [1, "First name is too short"],
+      //   maxLength: [60, "First name is too long"],
       trim: true,
     },
     mName: {
@@ -46,9 +46,9 @@ const UserSchema = mongoose.Schema(
     },
     lName: {
       type: String,
-    //   required: [true, "Last name is required"],
-    //   minLength: [1, "Last name is too short"],
-    //   maxLength: [60, "Last name is too long"],
+      //   required: [true, "Last name is required"],
+      //   minLength: [1, "Last name is too short"],
+      //   maxLength: [60, "Last name is too long"],
       trim: true,
     },
 
@@ -81,12 +81,12 @@ const UserSchema = mongoose.Schema(
       trim: true,
       // no maxlen here as we store hashed password which is too long, only maxlen on client side
       // todo: put a custom password policy validator here, google srch this, pass will be hashed and then stored therefore no maxLen
-    //   validate: {
-    //     validator: isPasswordPolicyFollowed,
-    //     message: (props) => {
-    //       return "Password policy is not followed";
-    //     },
-    //   },
+      //   validate: {
+      //     validator: isPasswordPolicyFollowed,
+      //     message: (props) => {
+      //       return "Password policy is not followed";
+      //     },
+      //   },
     },
 
     //////////////////////
@@ -96,22 +96,22 @@ const UserSchema = mongoose.Schema(
     },
     collegeName: {
       type: String,
-    //   required: true,
-    //   minLength: [1, "College name is too short"],
+      //   required: true,
+      //   minLength: [1, "College name is too short"],
       maxLength: [120, "College name is too long"],
       trim: true,
     },
     region: {
       type: String,
-    //   required: true,
-    //   minLength: [1, "Region is too short"],
+      //   required: true,
+      //   minLength: [1, "Region is too short"],
       maxLength: [60, "Region is too long"],
       trim: true,
     },
     branch: {
       type: String,
-    //   required: true,
-    //   minLength: [1, "Branch is too short"],
+      //   required: true,
+      //   minLength: [1, "Branch is too short"],
       maxLength: [60, "Branch is too long"],
       trim: true,
     },
@@ -120,7 +120,7 @@ const UserSchema = mongoose.Schema(
     // todo: phone maxlen
     phone: {
       type: String,
-    //   required: true,
+      //   required: true,
       minLength: [1, "Phone is too short"],
       maxLength: [20, "Phone is too long"],
       trim: true,
@@ -134,39 +134,46 @@ const UserSchema = mongoose.Schema(
     },
     addLine1: {
       type: String,
-    //   required: true,
-    //   minLength: [1, "Address Line 1 is too short"],
+      //   required: true,
+      //   minLength: [1, "Address Line 1 is too short"],
       maxLength: [120, "Address Line 1 is too long"],
       trim: true,
     },
     addLine2: {
       type: String,
-    //   required: true,
-    //   minLength: [1, "Address Line 2 is too short"],
+      //   required: true,
+      //   minLength: [1, "Address Line 2 is too short"],
       maxLength: [120, "Address Line 2 is too long"],
       trim: true,
     },
     city: {
       type: String,
-    //   required: true,
-    //   minLength: [1, "City name is too short"],
+      //   required: true,
+      //   minLength: [1, "City name is too short"],
       maxLength: [60, "City name is too long"],
       trim: true,
     },
     pincode: {
       type: String,
-    //   required: true,
-    //   minLength: [1, "Pincode is too short"],
+      //   required: true,
+      //   minLength: [1, "Pincode is too short"],
       maxLength: [10, "Pincode is too long"],
       trim: true,
     },
     country: {
       type: String,
-    //   required: true,
-    //   minLength: [1, "Country name is too short"],
+      //   required: true,
+      //   minLength: [1, "Country name is too short"],
       maxLength: [60, "Country name is too long"],
       trim: true,
     },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      enum: ["M", "F", "O"]
+    }
   },
   { minimize: false }
 );
